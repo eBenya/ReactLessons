74641b99ec263dcbf3fcb9373dd5b2cd36245476
eBenya <MyMail@com>
2021-05-25 13:48:18 +0300

    lesson 8-9. Using conditional constructs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 
 function App() {
-  const attr = 'block';
+  const isAdult = true;
+  const isAdmin = true;
+  let text;
+  let adminContent;
+
+  if (isAdmin) {
+    adminContent = (
+      <>
+        <h5>Admin content:</h5>
+        <p>text</p>
+      </>
+    );
+  }
+  if (isAdult) {
+    text = <p>Is once paragraph</p>
+  }
+  else {
+    text = <p>Is another paragraph</p>
+  }
+
+  const isAuth = false;
+
   return (
     <>
-      <div id={attr}>
-        text id
+      <div>
+        <h5>Content:</h5>
+        {text}
       </div>
-      <div className={attr}>
-        text class
+      <div>
+        {adminContent}
+      </div>
+      <div>
+        {!isAuth && <p>ternary operator</p>}
       </div>
     </>
   );

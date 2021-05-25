379ae4bd4f1436d4bc31a0a9524eb130eab98bae
eBenya <MyMail@com>
2021-05-25 13:25:07 +0300

    lesson 2. Insert value variabl and constant to JSX

--- src/App.js
+++ src/App.js
@@ -1,48 +1,33 @@
 import React from 'react';
 
 function App() {
-  const elem = (
-    <div>
-      <h3>someText</h3>
-      <p className="Paragraph1">Paragraph 1</p>
-      <p className="Paragraph2">Paragraph 2</p>
-      <p className="Paragraph3">Paragraph 3</p>
-      <input /><br /><input />
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
-            <th>Column 3</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>2</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>3</td>
-            <td>3</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+  const arr = [1, 2, 3, 4, 5];
+  const obj = { name: 'john', surname: 'smit' };
 
-  return elem;
+  return (
+    <>
+      <div>
+        <h5>Render result - tag ul that contains tag li with array elements:</h5>
+        <ul>
+          <li>arr[0]</li>
+          <li>arr[1]</li>
+          <li>arr[2]</li>
+          <li>arr[3]</li>
+          <li>arr[4]</li>
+        </ul>
+      </div>
+      <div>
+        <h5>Render object:</h5>
+        <p>
+          {'<p>'}<br />
+          name: {'<span>'}<span>{obj.name}</span>{'</span>'}, {'<br/>'}
+          <br />
+          surname: {'<span>'}<span>{obj.surname}</span>{'</span>'}
+          <br/>{'</p>'}
+        </p>
+      </div>
+    </>
+  )
 }
 
 export default App;

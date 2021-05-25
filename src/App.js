00a6ecf40fa12527d3259365bbd72f658914b629
eBenya <MyMail@com>
2021-05-25 13:32:27 +0300

    lesson 7. Insert members into JSX attribytes of react components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React from 'react';
 
 function App() {
-  const arr = [1, 2, 3, 4, 5];
-  const obj = { name: 'john', surname: 'smit' };
-
+  const attr = 'block';
   return (
     <>
-      <div>
-        <h5>Render result - tag ul that contains tag li with array elements:</h5>
-        <ul>
-          <li>arr[0]</li>
-          <li>arr[1]</li>
-          <li>arr[2]</li>
-          <li>arr[3]</li>
-          <li>arr[4]</li>
-        </ul>
+      <div id={attr}>
+        text id
       </div>
-      <div>
-        <h5>Render object:</h5>
-        <p>
-          {'<p>'}<br />
-          name: {'<span>'}<span>{obj.name}</span>{'</span>'}, {'<br/>'}
-          <br />
-          surname: {'<span>'}<span>{obj.surname}</span>{'</span>'}
-          <br/>{'</p>'}
-        </p>
+      <div className={attr}>
+        text class
       </div>
     </>
-  )
+  );
 }
 
 export default App;

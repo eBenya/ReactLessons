b2a3b3afd206b0ec64c0902e88052ec4222acc18
eBenya <MyMail@com>
2021-05-25 13:03:19 +0300

    firstLesson, reactComponent with html tags

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const elem = (
+    <div>
+      <h3>someText</h3>
+      <p className="Paragraph1">Paragraph 1</p>
+      <p className="Paragraph2">Paragraph 2</p>
+      <p className="Paragraph3">Paragraph 3</p>
+      <input /><br /><input />
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
+
+  return elem;
 }
 
 export default App;

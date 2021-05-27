ef11f90aa755dd388c63790d574fe2bd754343b2
eBenya <MyMail@com>
2021-05-27 17:36:17 +0300

    States. selectrs

--- src/App.js
+++ src/App.js
@@ -1,80 +1,50 @@
 import React, { useState } from 'react';
 
+
 function App() {
+	let cityes = ['city_1', 'city_2', 'city_3', 'city_4', 'city_5', 'city_6', 'city_7', 'city_8', 'city_9', 'city_0'];
+	let [city, setCity] = useState('');
+
+	function cityRender(name, i) {
+		return (
+			<option key={i}>{name}{i}</option>
+		);
+	}
+
+	const opts = cityes.map((content, i) => {
+		return <option key={i} value={i}>{content}</option> // cityRender(content, i);
+	});
 
 	const [value, setValue] = useState('');
-
-	function handleChange(event) {
-		setValue(event.target.value);
-		sumAllNumbers();
-	}
-
-	let [sum, setSum] = useState(0);
-	function sumAllNumbers() {
-		let arr = value.split(' ');
-		let res = arr.map(x => Number.isNaN(Number(x)) ? 0 : Number(x))
-			.reduce((sum, i) => sum += i);
-		setSum(res);
-	}
-
-	let [isOnline, setIsOnline] = useState(false);
-	let [mess, setMess] = useState('');
-	function writeToUser() {
-		if (isOnline) {
-			setMess('Hi');
-		}
-		else {
-			setMess('Bye');
-		}
-	}
-
-	const htmlName = "HTML";
-	const cssName = "CSS";
-	const jsName = "JS";
-	let [knowCSS, setKnowCSS] = useState(false);
-	let [knowJS, setKnowJS] = useState(false);
-	let [knowHTML, setKnowHTML] = useState(false);
-
-	let [isAdult, setIsAdult] = useState(false);
-	let [message, setMessage] = useState('');
-	function adultChangeHandler() {
-		setIsAdult(!isAdult);
-
-		if (!isAdult) {
-			setMessage('Yes');
-		}
-		else {
-			setMessage('No');
-		}
-	}
 	return (
 		<>
 			<div className="borderedDiv">
-				<textarea value={value} onChange={handleChange} />
-				<p>{value}</p>
-				<p>Text length: {value.length}</p>
-				<p>Sum all numbers = {sum}</p>
-			</div>
-			<div className="borderedDiv">
-				<input type="checkbox" checked={isOnline} onChange={() => setIsOnline(!isOnline)} />
-				<button onClick={writeToUser}>write</button>
-				<p>{mess}</p>
-			</div>
-			<div className="borderedDiv">
-				{htmlName}:<input type="checkbox" checked={knowHTML} onChange={() => setKnowHTML(!knowHTML)} placeholder="HTML:"></input>
-				{cssName}:<input type="checkbox" checked={knowCSS} onChange={() => setKnowCSS(!knowCSS)} placeholder="CSS:"></input>
-				{jsName}:<input type="checkbox" checked={knowJS} onChange={() => setKnowJS(!knowJS)} placeholder="JS:"></input>
+				<select value={city} onChange={(e) => setCity(e.target.value)}>
+					{opts}
+				</select>
 				<p>
-					I know: {knowHTML ? (htmlName + ';') : ''} {knowCSS ? (cssName + ';') : ''} {knowJS ? (jsName + ';') : ''}
+					Yot put: {city}	<br/>
+					It number: {city} <br/>
+					It value: {cityes[city]}
 				</p>
 			</div>
 			<div className="borderedDiv">
-				Are you 18? <input type="checkbox" checked={isAdult} onChange={adultChangeHandler} />
-				 <h2>{message}</h2>				
-				<p hidden={!isAdult}>
-					Adut content...
+				Your age group:
+				<select value={value} onChange={event => setValue(event.target.value)}>
+					<option value="1">0-12</option>
+					<option value="2">13-17</option>
+					<option value="3">18-25</option>
+					<option value="4">over 25</option>
+				</select>
+				<p>
+					your age group 
+					{value === '1' && ' from 0 to 12'}
+					{value === '2' && ' from 12 to 17'}
+					{value === '3' && ' from 18 to 25'}
+					{value === '4' && ' over 25'}
 				</p>
 			</div>
+
 		</>
 	);
 }

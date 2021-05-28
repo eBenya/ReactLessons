63282d918e205a508d4fc7b03f94c7ec2e5f3fee
eBenya <koly_07@mail.ru>
2021-05-28 12:59:16 +0300

    ReactArrays introduction

--- src/App.js
+++ src/App.js
@@ -2,55 +2,79 @@ import React, { useState } from 'react';
 
 
 function App() {
-	const arr = ['', '', ''];
-	const languages = ['HTML', 'CSS', 'JS'];
-	const [value, setValue] = useState(1);
-	const [language, setLanguage] = useState(1);
+	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
 
-	function renderRadio(nameR, val, iterator, state, setVal) {
-		return (
-			<span key={iterator}>
-				{val}
-				<input
-					type="radio"
-					name={nameR}
-					value={iterator}
-					checked={state == iterator ? true : false}
-					onChange={(e) => setVal(e.target.value)}
-				/>{'   '}
-			</span>
-		);
+	const res = notes.map((val, i) => {
+		return <p key={i}>{val}</p>;
+	});
+
+	function addElement() {
+		setNotes([...notes, notes.length + 1]);
 	}
-	let op = arr.map((val, i) => {
-		return renderRadio('radio', val, i, value, setValue);
-	});
-	let langs = languages.map((val, i) => {
-		return renderRadio('lang', val, i, language, setLanguage);
-	});
+	function removeElement() {
+		let temp = notes.slice();
+		temp.pop();
+		setNotes(temp);
+	}
+	let [elementNumber, setElementNumber] = useState(0);
+	let [newValue, setNewValue] = useState(0);
+	function removeConcreeteElement() {
+		let temp = notes.slice();
+		temp.splice(elementNumber, 1);
+		setNotes(temp);
+		//setNotes([...notes.slice(0, elementNumber), ...notes.slice(elementNumber,1)]); // Надо изучить деструтуризацию!!!
+	}
+	function editElement() {
+		let temp = notes.slice();
+		temp[elementNumber] = newValue;
+		setNotes(temp);
+	}
+	function sort() {
+		let temp = notes.slice().sort();
+		setNotes(temp);
+	}
+	function reverse() {
+		let temp = notes.slice().reverse();
+		setNotes(temp);
+	}
 
-	const [deffault, setDeffault] = useState('kek');
-
+	const [charArr, setCharArr] = useState(['a', 'b', 'c', 'd', 'e']);
+	const [inputText, setInputText] = useState('');
+	const ULContent = charArr.map((val, i) => {
+		return (
+			<li key={i}>{val} <button onClick={()=>removeIndex(i)}>remove</button></li>
+		);
+	});
+	function addULElemnt() {
+		setCharArr([...charArr, inputText])
+		setInputText('');
+	}
+	function removeIndex(index){
+		let temp = charArr.slice()
+		temp.splice(index, 1);
+		setCharArr(temp);
+	}
 	return (
 		<>
 			<div className="borderedDiv">
-				<p>
-					{op} <br />
-					You put: {Number(value) + 1}
-				</p>
-			</div>
-			<div className="borderedDiv">
-				<p>
-					{langs} <br />
-						Your favorite language:{languages[language]} <br />
-					{languages[language] === languages[2] ? 'Krasauchegg!!!' : ''}
-				</p>
-			</div>
-			<div className="borderedDiv">
-				Set value without OnChange:<input value={deffault}/> <br/>
-				Set defaultValue without OnChange:<input defaultValue={deffault}/> <br/><br/>
+				<button onClick={addElement}>add</button>
+				<button onClick={removeElement}>remove last</button> <br />
 
-				Set value with OnChange:<input value={deffault} onChange={e=>setDeffault(e.target.value)}/> <br/>
-				Set defaultValue with OnChange:<input defaultValue={deffault} onChange={e=>setDeffault(e.target.value)}/><br/>
+				number of element: <input type="number" value={elementNumber} onChange={e => setElementNumber(e.target.value)} /> <br />
+				new value of element: <input type="number" value={newValue} onChange={e => setNewValue(e.target.value)} /> <br />
+				<button onClick={removeConcreeteElement}>remove element</button>
+				<button onClick={editElement}>edit element</button> <br />
+				<button onClick={sort}>sort</button>
+				<button onClick={reverse}>reverse</button>
+				{res}
+			</div>
+
+			<div className="borderedDiv">
+				<input value={inputText} onBlur={addULElemnt} onChange={e => setInputText(e.target.value)} />
+				<br />
+				<ul>
+					{ULContent}
+				</ul>
 			</div>
 		</>
 	);

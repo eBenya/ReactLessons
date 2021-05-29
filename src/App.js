f37b15dade311593bd65208c395ab57234516259
eBenya <koly_07@mail.ru>
2021-05-29 13:44:27 +0300

    Bind input with react obj

--- src/App.js
+++ src/App.js
@@ -3,75 +3,53 @@ import React, { useState } from 'react';
 
 function App() {
 
-	const [notes, setNotes] = useState([1, 2, 3, 4]);
-	const renderNotes = notes.map((val, i) => {
-		return <input key={i} value={val} onChange={e => setNote(e, i)} />
+	const [obj, setObj] = useState({
+		prop1: 'value1',
+		prop2: 'value2',
+		prop3: 'value3',
 	});
-	function getSum(arr) {
-		let sum = 0;
-
-		for (const elem of arr) {
-			sum += +elem;
-		}
-
-		return sum;
-	}
-	function setNote(event, iter) {
-		var temp = notes.slice();
-		temp[iter] = event.target.value;
-		setNotes(temp);
+	function changeObj(obj, propName, setFunc, event) {
+		let temp = Object.assign({}, obj);
+		temp[propName] = event.target.value;
+		setFunc(temp);
 	}
 
-	const strings = ['kek', 'a', 'b', 'c', 'd', 'e'];
-	const [notes2, setNotes2] = useState(strings);
-	const [editNote, setEditNote] = useState(null);
-	const renderNotes2 = notes2.map((val, i) => {
-		return (
-			<li key={i} onClick={() => editVal(i)}>{val} </li>
-		);
-	})
-	const renderUL = (<ul>{renderNotes2}</ul>)
-	function editVal(iter) {
-		setEditNote({ id: iter, val: notes2[iter] });
-	}
-	function endEdit() {
-		let temp = notes2.slice();
-
-		if (editNote != null && editNote.id != null) {
-			if (editNote.val === '') {
-				temp.splice(editNote.id, 1);
-			}
-			else {
-				temp[editNote.id] = editNote.val;
-			}
-		}
-		else {
-			temp.push(editNote.val);
-		}
-		setNotes2(temp);
-		setEditNote(null);
-	}
-	function onChangeInput(event) {
-		if (editNote != null) {
-			setEditNote({ ...editNote, val: event.target.value });
-		}
-		else {
-			setEditNote({ id: null, val: event.target.value });
+	const initDate = {
+		year: 2025,
+		month: 12,
+		day: 31,
+		toString(){
+			return  this.year+'-'+this.month+'-'+this.day;
 		}
 	}
+	const [date, setDate] = useState(initDate);
 
 	return (
 		<>
 			<div className="borderedDiv">
-				{renderNotes}
-				<br />
-				Sum = {getSum(notes)}
+				<span>{obj.prop1}</span> <button onClick={() => setObj({ ...obj, ...{ prop1: '!' } })}>change</button> <br />
+				<span>{obj.prop2}</span> <button onClick={() => setObj({ ...obj, ...{ prop2: '@' } })}>change</button> <br />
+				<span>{obj.prop3}</span> <button onClick={() => setObj({ ...obj, ...{ prop3: '#' } })}>change</button> <br />
 			</div>
 
 			<div className="borderedDiv">
-				Put one of the next elements:<br />
-				{renderUL}
-				<input value={editNote !== null ? editNote.val : ''} onChange={e => onChangeInput(e)} onBlur={endEdit} />
+				<input value={obj.prop1} onChange={e => changeObj(obj, 'prop1', setObj, e)} />
+				<input value={obj.prop2} onChange={e => changeObj(obj, 'prop2', setObj, e)} />
+				<input value={obj.prop3} onChange={e => changeObj(obj, 'prop3', setObj, e)} />
+				<br />
+				{obj.prop1}-{obj.prop2}-{obj.prop3}
+			</div>
+
+			<div className="borderedDiv">
+				<p>
+					year: {date.year}; moth: {date.month}; day: {date.day};
+					day of weak: {(new Date(date.toString())).getDay()};
+					<br/>
+					Edit fields:<br/>
+					<input value={date.year} onChange={e=>changeObj(date, 'year', setDate, e)} placeholder="year"/>
+					<input value={date.month} onChange={e=>changeObj(date, 'month', setDate, e)} placeholder="month"/>
+					<input value={date.day} onChange={e=>changeObj(date, 'day', setDate, e)} placeholder="day"/>
+				</p>
 			</div>
 		</>
 	);

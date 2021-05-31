675d1aba61d8fe6e6499d51016d322de45cd34ce
eBenya <koly_07@mail.ru>
2021-05-31 19:27:27 +0300

    Editin elements with apearing input

--- src/App.js
+++ src/App.js
@@ -7,141 +7,114 @@ function id() {
 }
 
 const initNotes = [
-	{
-		id: id(),
-		name: 'name1',
-		desc: 'long description 1',
-		showDescr: false,
-	},
-	{
-		id: id(),
-		name: 'name2',
-		desc: 'long description 2',
-		showDescr: false,
-	},
-	{
-		id: id(),
-		name: 'name3',
-		desc: 'long description 3',
-		showDescr: false,
-	},
-];
-
-const initProds = [
-	{
-		id: id(),
-		name: 'prod1',
-		cost: 'cost1',
-		desc: 'long description 1',
-		comm: 'my super comment 1',
-		showDescr: false,
-		showComm: false
-	},
-	{
-		id: id(),
-		name: 'prod2',
-		desc: 'long description 2',
-		comm: 'my super comment 2',
-		showDescr: false,
-		showComm: false
-	},
-	{
-		id: id(),
-		name: 'prod3',
-		desc: 'long description 3',
-		comm: 'my super comment 3',
-		showDescr: false,
-		showComm: false
-	},
+	{ text: 'note1', isEdit: false },
+	{ text: 'note2', isEdit: false },
+	{ text: 'note3', isEdit: true },
 ];
 
 function App() {
-	const [visible1, setVisible1] = useState(false);
-	let elem1;
-	if (visible1) {
-		elem1 = <p>text1</p>;
+	const [value, setValue] = useState('text');
+	const [isEdit, setIsEdit] = useState(true);
+
+	let par;
+	if (isEdit) {
+		par = <p><input value={value} onChange={e => setValue(e.target.value)} onBlur={() => setIsEdit(!isEdit)} /></p>
+	}
+	else {
+		par = <p onClick={() => setIsEdit(!isEdit)}>{value}</p>
 	}
 
-	const [visible2, setVisible2] = useState(false);
-	let elem2;
-	if (visible2) {
-		elem2 = <p>text2</p>;
-	}
+	const [value2, setValue2] = useState('text2');
+	const [isEdit2, setIsEdit2] = useState(false);
 
-	const [visible3, setVisible3] = useState(false);
-	let elem3;
-	if (visible3) {
-		elem3 = <p>text3</p>;
+	let par2;
+	if (isEdit2) {
+		par2 = <p>
+			<input value={value2} onChange={e => setValue2(e.target.value)} /><br />
+			<button onClick={() => setIsEdit2(true)}>Edit</button>
+			<button onClick={() => setIsEdit2(false)}>View</button>
+		</p>
+	}
+	else {
+		par2 = <p>
+			{value2}<br />
+			<button onClick={() => setIsEdit2(true)}>Edit</button>
+			<button onClick={() => setIsEdit2(false)}>View</button>
+		</p>;
 	}
 
 	const [notes, setNotes] = useState(initNotes);
-	const renderNotes = notes.map(x => {
-		let desc;
-		if (x.showDescr) {
-			desc = <i>{x.desc}</i>;
+	const result = notes.map((note, index) => {
+		let elem;
+		if(note.isEdit){
+			elem = <input value={note.text} onChange={e=>changeInput(index, e, notes, setNotes)} onBlur={()=>endEdit(index, notes, setNotes)}/>
 		}
-		return (
-			<p key={x.id}>
-				{x.name},
-				<button onClick={() => ShowDescr(x.id, notes, setNotes)}>{x.showDescr ? 'Hide' : 'Show'}</button>
-				{desc}
-			</p>
+		else{
+			elem = <span onClick={()=>startEdit(index, notes, setNotes)}>{note.text}</span>
+		}
+		return <li key={index}>{elem}</li>;
+	});
+
+	function startEdit(index, objs, func){
+		var temp = Object.assign([], objs);
+		temp[index].isEdit = true;
+		func(temp);
+	}
+	function endEdit(index, objs, func){
+		var temp = Object.assign([], objs);
+		temp[index].isEdit = false;
+		func(temp);
+	}
+	function changeInput(index, event, objs, func){
+		var temp = Object.assign([], objs);
+		temp[index].text = event.target.value;
+		func(temp);
+	}
+
+	const [notes2, setNotes2] = useState(initNotes);
+	const renderNotes2 = notes2.map((note, index)=>{
+		let el;
+		if(note.isEdit){
+			el = <input value={note.text} onChange={e=>changeInput(index, e, notes2, setNotes2)}/>
+		}
+		else{
+			el = <span>{note.text}</span>
+		}
+		
+		return(
+			<li key={index}>
+				{el}
+				<button onClick={()=>startOrEndEdit(index, notes2, setNotes2)}>{note.isEdit ? 'End edit' : 'Start edit'}</button>
+			</li>
 		);
 	});
-	function ShowDescr(id, objs, func) {
-		func(objs.map(obj => obj.id === id ? { ...obj, showDescr: !obj.showDescr } : obj));
+	function startOrEndEdit(index, objss, func){
+		var temp = Object.assign([], objss);
+		temp[index].isEdit = !temp[index].isEdit;
+		func(temp);
 	}
-
-	const [products, setProducts] = useState(initProds);
-	const renderProducts = <ul>
-		{
-			products.map(x => {
-				let desc;
-				if (x.showDescr) {
-					desc = <i>{x.desc}</i>;
-				}
-
-				let comm;
-				if (x.showComm) {
-					comm = <i>{x.comm}</i>;
-				}
-
-				return <li key={x.id}>
-					{x.name}
-					<button onClick={() => ShowDescr(x.id, products, setProducts)}>Description:</button>
-					{desc}
-					<button onClick={() => ShowComm(x.id, products, setProducts)}>Comments:</button>
-					{comm}
-				</li>
-			})
-		}
-	</ul>;
-
-	function ShowComm(id, objs, func) {
-		func(objs.map(obj => obj.id === id ? { ...obj, showComm: !obj.showComm } : obj));
-	}
-
 
 	return (
 		<>
 			<div className="borderedExaplesLessonsFromTasks">
 				<div className="borderedDiv">
-					<button onClick={() => setVisible1(!visible1)}>{visible1 ? 'Hide 1' : 'Show 1'}</button>
-					<button onClick={() => setVisible2(!visible2)}>{visible2 ? 'Hide 2' : 'Show 2'}</button>
-					<button onClick={() => setVisible3(!visible3)}>{visible3 ? 'Hide 3' : 'Show 3'}</button>
-					{elem1}
-					{elem2}
-					{elem3}
+					{par}
 				</div>
-
 				<div className="borderedDiv">
-					{renderNotes}
+					<ul>
+						{result}
+					</ul>
 				</div>
 			</div>
 
 			<div className="borderedExaplesLessonsFromTasks">
 				<div className="borderedDiv">
-					{renderProducts}
+					{par2}
+				</div>
+				<div className="borderedDiv">
+					<ul>
+						{renderNotes2}
+					</ul>
 				</div>
 			</div>
 		</>

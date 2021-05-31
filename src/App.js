743756c088368a9d640b9fcf37925c67ad43d0c2
eBenya <koly_07@mail.ru>
2021-05-31 16:29:42 +0300

    show/hide content

--- src/App.js
+++ src/App.js
@@ -2,163 +2,146 @@ import { render } from '@testing-library/react';
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 
-const initNotes = [
-	{
-		id: 'GYi9G_uC4gBF1e2SixDvu',
-		prop1: 'value11',
-		prop2: 'value12',
-		prop3: 'value13',
-	},
-	{
-		id: 'IWSpfBPSV3SXgRF87uO74',
-		prop1: 'value21',
-		prop2: 'value22',
-		prop3: 'value23',
-	},
-	{
-		id: 'JAmjRlfQT8rLTm5tG2m1L',
-		prop1: 'value31',
-		prop2: 'value32',
-		prop3: 'value33',
-	},
-];
-const initProds = [
-	{ id: getNewId(), name: 'prod1', catg: 'catg1', cost: 100 },
-	{ id: getNewId(), name: 'prod2', catg: 'catg2', cost: 200 },
-	{ id: getNewId(), name: 'prod3', catg: 'catg3', cost: 300 },
-];
-
-function getNewId() {
+function id() {
 	return nanoid();
 }
 
+const initNotes = [
+	{
+		id: id(),
+		name: 'name1',
+		desc: 'long description 1',
+		showDescr: false,
+	},
+	{
+		id: id(),
+		name: 'name2',
+		desc: 'long description 2',
+		showDescr: false,
+	},
+	{
+		id: id(),
+		name: 'name3',
+		desc: 'long description 3',
+		showDescr: false,
+	},
+];
+
+const initProds = [
+	{
+		id: id(),
+		name: 'prod1',
+		cost: 'cost1',
+		desc: 'long description 1',
+		comm: 'my super comment 1',
+		showDescr: false,
+		showComm: false
+	},
+	{
+		id: id(),
+		name: 'prod2',
+		desc: 'long description 2',
+		comm: 'my super comment 2',
+		showDescr: false,
+		showComm: false
+	},
+	{
+		id: id(),
+		name: 'prod3',
+		desc: 'long description 3',
+		comm: 'my super comment 3',
+		showDescr: false,
+		showComm: false
+	},
+];
+
 function App() {
-	const [products, setProducts] = useState(initProds);
+	const [visible1, setVisible1] = useState(false);
+	let elem1;
+	if (visible1) {
+		elem1 = <p>text1</p>;
+	}
+
+	const [visible2, setVisible2] = useState(false);
+	let elem2;
+	if (visible2) {
+		elem2 = <p>text2</p>;
+	}
+
+	const [visible3, setVisible3] = useState(false);
+	let elem3;
+	if (visible3) {
+		elem3 = <p>text3</p>;
+	}
+
 	const [notes, setNotes] = useState(initNotes);
-	const [inputState, setInputState] = useState(null);
-
-	function renderTableHead(obj) {
-		return (
-			<thead>
-				<tr>
-					{Object.keys(obj).map((val, it) => <th key={it}>{val}</th>)}
-				</tr>
-			</thead>
-		);
-	}
-	function renderTableRow(obj, elementKey, stateObjs, changeStateFunction, inputState, inputStateChangeFunc) {
-		return (
-			<tr key={elementKey}>
-				{Object.entries(obj).map(x => <td key={x[0]}>{x[1]}</td>)}
-				{
-					changeStateFunction ?
-						<td>
-							<button onClick={() => delElementOnNumber(stateObjs, elementKey, changeStateFunction)}>del</button>
-
-							{
-								((inputState || inputState === null) && inputStateChangeFunc) ? <button onClick={() => inputStateChangeFunc(obj)}>Edit</button> : ''
-							}
-
-						</td>
-						: null
-				}
-			</tr>
-		);
-	}
-	function renderTableBody(objects, changeStateFunction, inputState, inputStateChangeFunc) {
-		return (
-			<tbody>
-				{objects.map((val, it) => renderTableRow(val, it, objects, changeStateFunction, inputState, inputStateChangeFunc))}
-			</tbody>
-		);
-	}
-	function renderAddOrEditForm(objsCollection, changeObjStateFunc, inputState, inputStateChangeFunc) {
-		return (
-			<>
-				{
-					Object.entries(objsCollection[0])
-						.map(x => {
-							if (x[0] !== 'id') {
-								return (
-									<span key={x[0]}>
-										{x[0]}:
-										<input
-											value={inputState !== null ? inputState[x[0]] : ''}
-											onChange={e => changePropInputFormObj(inputState, inputStateChangeFunc, x[0], e.target.value, objsCollection[0])}
-										/>{' '}
-									</span>
-								);
-							}
-						})
-				}
-				<button onClick={() => addOrEditElement(objsCollection, changeObjStateFunc, inputState, inputStateChangeFunc)}>Add/Edit</button>
-			</>
-		);
-	}
-	function changePropInputFormObj(objState, changeFunctionState, keyName, newValue, supportObj) {
-		let temp = {};
-		for (let key in supportObj) {
-			if (objState != null && key in objState) {
-				temp[key] = objState[key];
-			}
-			else {
-				temp[key] = '';
-			}
+	const renderNotes = notes.map(x => {
+		let desc;
+		if (x.showDescr) {
+			desc = <i>{x.desc}</i>;
 		}
-		temp[keyName] = newValue;
-		changeFunctionState(temp);
-	}
-	function renderTable(objects, changeStateFunction, inputObjState, changeInputFormFunc) {
-		if (objects.length > 0) {
-			return (
-				<>
-					{((inputObjState || inputObjState === null) && changeInputFormFunc && changeStateFunction) ? renderAddOrEditForm(objects, changeStateFunction, inputObjState, changeInputFormFunc) : null}
-					<table border="1px">
-						{renderTableHead(objects[0])}
-						{renderTableBody(objects, changeStateFunction, inputObjState, changeInputFormFunc)}
-					</table>
-				</>
-			)
-		}
-		return <span>404 NotFound</span>
+		return (
+			<p key={x.id}>
+				{x.name},
+				<button onClick={() => ShowDescr(x.id, notes, setNotes)}>{x.showDescr ? 'Hide' : 'Show'}</button>
+				{desc}
+			</p>
+		);
+	});
+	function ShowDescr(id, objs, func) {
+		func(objs.map(obj => obj.id === id ? { ...obj, showDescr: !obj.showDescr } : obj));
 	}
 
-	function delElementOnNumber(stateObj, key, changeStateFunc) {
-		let temp = stateObj.slice();
-		temp.splice(key, 1);
-		changeStateFunc(temp);
-	}
-	function addOrEditElement(objCollection, changeCollectionStateFunc, newObj, clearInputFunc) {
-		if (newObj != null) {
-			if ('id' in newObj) {
-				if (newObj.id === null || newObj.id === '') {
-					newObj.id = getNewId();
-					//AddNew
-					changeCollectionStateFunc([...objCollection, newObj]);
+	const [products, setProducts] = useState(initProds);
+	const renderProducts = <ul>
+		{
+			products.map(x => {
+				let desc;
+				if (x.showDescr) {
+					desc = <i>{x.desc}</i>;
 				}
-				else {
-					//Edit
-					changeCollectionStateFunc(objCollection.map(obj => obj.id === newObj.id ? newObj : obj));
+
+				let comm;
+				if (x.showComm) {
+					comm = <i>{x.comm}</i>;
 				}
-			}
-			// If obj have`t id, then we can only add element, witout edit exist.
-			else {
-				changeCollectionStateFunc([objCollection, newObj]);
-			}
-			clearInputFunc(null);
+
+				return <li key={x.id}>
+					{x.name}
+					<button onClick={() => ShowDescr(x.id, products, setProducts)}>Description:</button>
+					{desc}
+					<button onClick={() => ShowComm(x.id, products, setProducts)}>Comments:</button>
+					{comm}
+				</li>
+			})
 		}
+	</ul>;
+
+	function ShowComm(id, objs, func) {
+		func(objs.map(obj => obj.id === id ? { ...obj, showComm: !obj.showComm } : obj));
 	}
+
 
 	return (
 		<>
-			<h3>Lesson examples:</h3>
-			<div className="borderedExaplesLessonsFromTasks">
-				{renderTable(notes)}
-			</div>
-			<h3>Tasks:</h3>
 			<div className="borderedExaplesLessonsFromTasks">
 				<div className="borderedDiv">
-					{renderTable(products, setProducts, inputState, setInputState)}
+					<button onClick={() => setVisible1(!visible1)}>{visible1 ? 'Hide 1' : 'Show 1'}</button>
+					<button onClick={() => setVisible2(!visible2)}>{visible2 ? 'Hide 2' : 'Show 2'}</button>
+					<button onClick={() => setVisible3(!visible3)}>{visible3 ? 'Hide 3' : 'Show 3'}</button>
+					{elem1}
+					{elem2}
+					{elem3}
+				</div>
+
+				<div className="borderedDiv">
+					{renderNotes}
+				</div>
+			</div>
+
+			<div className="borderedExaplesLessonsFromTasks">
+				<div className="borderedDiv">
+					{renderProducts}
 				</div>
 			</div>
 		</>
